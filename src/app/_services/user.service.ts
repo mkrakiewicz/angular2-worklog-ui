dc95ecf982d2cd7e489622a35a81d375934ab974
Michał Krakiewicz <michalkrakiewicz@hotmail.com>
2017-08-12 17:15:51 +0200

    Amendments

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -8,33 +8,35 @@ export class UserService {
     constructor(private http: Http) { }
 
     getAll() {
-        return this.http.get('/api/users', this.jwt()).map((response: Response) => response.json());
+        //noinspection TypeScriptValidateTypes
+        return this.http.get('http://laravel-worklog-calculator.public.dev/api/users', this.jwt()).map((response: Response) => response.json().data);
     }
 
     getById(id: number) {
-        return this.http.get('/api/users/' + id, this.jwt()).map((response: Response) => response.json());
+        return this.http.get('http://laravel-worklog-calculator.public.dev/api/users/' + id, this.jwt()).map((response: Response) => response.json().data);
     }
 
     create(user: User) {
-        return this.http.post('/api/users', user, this.jwt()).map((response: Response) => response.json());
+        return this.http.post('http://laravel-worklog-calculator.public.dev/api/users', user, this.jwt()).map((response: Response) => response.json().data);
     }
 
     update(user: User) {
-        return this.http.put('/api/users/' + user.id, user, this.jwt()).map((response: Response) => response.json());
+        return this.http.put('http://laravel-worklog-calculator.public.dev/api/users/' + user.id, user, this.jwt()).map((response: Response) => response.json().data);
     }
 
     delete(id: number) {
-        return this.http.delete('/api/users/' + id, this.jwt()).map((response: Response) => response.json());
+        return this.http.delete('http://laravel-worklog-calculator.public.dev/api/users/' + id, this.jwt()).map((response: Response) => response.json().data);
     }
 
     // private helper methods
 
     private jwt() {
         // create authorization header with jwt token
+        let headers = new Headers({'Content-Type': 'application/json'});
         let currentUser = JSON.parse(localStorage.getItem('currentUser'));
         if (currentUser && currentUser.token) {
-            let headers = new Headers({ 'Authorization': 'Bearer ' + currentUser.token });
-            return new RequestOptions({ headers: headers });
+            headers.append('Authorization','Bearer ' + currentUser.token);
         }
+        return new RequestOptions({ headers: headers });
     }
 }
